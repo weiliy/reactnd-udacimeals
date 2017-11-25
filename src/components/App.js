9908bb998c76fa4e1fe8a49029b0d751c421a730
Weili Yi <yi@weiliy.net>
2017-11-25 16:58:36 +0800

    Using the Provider

--- src/components/App.js
+++ src/components/App.js
@@ -2,43 +2,15 @@ import React, { Component } from 'react';
 import { addRecipe } from '../actions';
 
 class App extends Component {
-  state = {
-    calendar: null,
-  }
 
-  componentDidMount () {
-    const { store } = this.props;
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState(),
-      }));
-    });
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value,
-      },
-    }));
-
-    this.input.value = '';
-  }
   render() {
     return (
       <div>
-        <input
-          type='text'
-          ref={input => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
+        Hellow World!
       </div>
     );
   }
+
 }
 
 export default App;
